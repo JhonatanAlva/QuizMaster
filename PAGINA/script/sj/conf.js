65dc9d9f27497232c352cbca7179f376904a04ee
GuilleGG19 <85462361+GuilleGG19@users.noreply.github.com>
2024-05-05 14:18:10 -0600

    QZMSTR2-9 feature3

---
+++ PAGINA/script/sj/conf.js
@@ -0,0 +1,23 @@
+  // Función para cambiar de categoría
+  function cambiarCategoria(categoria) {
+    console.log("Cambiando la categoría: " + categoria)
+    //lógica adicional para cambiar la categoría
+  }
+
+  // Función para manejar el nombre de usuario
+  function mostrarNombreUsuario(nombreUsuario) {
+    document.getElementById("usuario").innerText = nombreUsuario
+  }
+
+  // Simula la obtención del nombre de usuario (reemplaza esto con tu lógica real)
+  var nombreUsuario =  localStorage.getItem("nameUserLogi");
+
+  // Muestra el nombre de usuario al cargar la página
+  window.addEventListener("DOMContentLoaded", function() {
+    mostrarNombreUsuario(nombreUsuario)
+  });
+
+  // Manejar el evento de clic en el botón de salida (si lo tienes)
+  document.getElementById("logoutButton").addEventListener("click", function() {
+    // la lógica para cerrar sesión
+  });
